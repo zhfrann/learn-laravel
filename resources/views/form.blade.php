14ec11a9ac20f3f1e4a15d15babd7f52cbae2dae
zhfrann <muhzhafranilham13@gmail.com>
2025-04-29 01:28:47 +0700

    laravel csrf

---
+++ resources/views/form.blade.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <form action="/form" method="post">
+        <label for="name">
+            <input type="text" name="name">
+        </label>
+        <input type="submit" value="Say Hello">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    </form>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CookieController;
 use App\Http\Controllers\FileController;
+use App\Http\Controllers\FormController;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\InputController;
 use App\Http\Controllers\RedirectController;
@@ -116,3 +117,8 @@ Route::get("/middleware/api", function () {
 Route::get("/middleware/group", function () {
     return "GROUP";
 })->middleware('pzn');
+
+
+// Csrf
+Route::get("/form", [FormController::class, "form"]);
+Route::post("/form", [FormController::class, "submitForm"]);
